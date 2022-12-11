ca48228b45c33d5aa536ed54e2bd2d3b9056ae55
michael <97482177+notmichaellopez@users.noreply.github.com>
2022-12-11 02:50:48 -0700

    Add files via upload
    DataDisplay.py
    Create a Covid Dashboard that shows: 
    1. Table 1 shows a table of the top 100 countries with the following columns:
        - Country Name
        - Total Deaths
        - New Deaths
        - Total Deaths / 1 Million
        - New Deaths / 1 Million
    
    2. Figure 1: An interactive world map which calls out:
        - Country name
        - New Deaths
        - Cumulative Deaths
    for each of the 100 countries
    
    Figure 2: An interactive plot of the top 5 countries with the highest number of new cases

---
+++ DataDisplay.py
@@ -0,0 +1,109 @@
+from bokeh.plotting import figure, output_file
+from bokeh.models import ColumnDataSource, TableColumn, DataTable,GeoJSONDataSource, HoverTool,ColorBar, NumeralTickFormatter
+from bokeh.layouts import gridplot, layout
+from bokeh.tile_providers import get_provider, Vendors
+from bokeh.palettes import OrRd, RdYlGn, TolYlOrBr
+from bokeh.transform import linear_cmap,factor_cmap
+from bokeh.io import show
+from datetime import date, timedelta
+import pandas as pd
+import numpy as np
+# Run ScrapeWebsite.py in order to get new data
+
+
+
+# Load the last three json files of data using pandas
+df1 = pd.read_json(r"C:\Users\lopez\Desktop\CovidDashboard-main\covid_data_2022-12-02.json")
+df2 = pd.read_json(r"C:\Users\lopez\Desktop\CovidDashboard-main\covid_data_2022-12-01.json")
+df3 = pd.read_json(r"C:\Users\lopez\Desktop\CovidDashboard-main\covid_data_2022-12-10.json")
+
+# TABLE 1: Table giving data from yesterday about all the columns collected
+# Tell Bokeh to use df1 as our source for the table                                                                                                  
+source = ColumnDataSource(df1)
+# Enter the table data and titles
+columns = [
+    TableColumn(field= 'Country',title= 'Country'),
+    TableColumn(field= 'Total Deaths',title= 'Total Country Deaths'),
+    TableColumn(field= 'New Deaths',title= 'New Deaths'),
+    TableColumn(field= 'Deaths/1M', title= 'Deaths / 1M'),
+    TableColumn(field= 'New Deaths/1M', title= 'New Deaths / 1M')
+]
+t1 = DataTable(source=source,columns=columns)
+
+# FIGURE 1: World map with interactive new deaths by hovering 
+# Load the coordinate data from countries.csv
+df4 = pd.read_csv('updated_countries.csv',encoding='latin-1')
+# Run the updated_countries script to get the coordinates of the top 100 covid countries
+
+
+
+# Define function to switch from lat/long to mercator coordinates
+#    x_coord function sourced from: https://github.com/nadinezab
+def x_coord(x, y):
+
+    lat = x
+    lon = y
+    
+    r_major = 6378137.000
+    x = r_major * np.radians(lon)
+    scale = x/lon
+    y = 180.0/np.pi * np.log(np.tan(np.pi/4.0 + 
+        lat * (np.pi/180.0)/2.0)) * scale
+    return (x, y)
+# Define the coords as tuple (lat,long)
+df4['coordinates'] = list(zip(df4['latitude'], df4['longitude']))
+# Obtain list of mercator coordinates
+mercators = [x_coord(x, y) for x, y in df4['coordinates'] ]
+# Create mercator column in our df
+df4['mercator'] = mercators
+# Split that column out into two separate columns - mercator_x and mercator_y
+df4[['mercator_x', 'mercator_y']] = df4['mercator'].apply(pd.Series)
+# Select tile set to use
+chosentile = get_provider(Vendors.STAMEN_TONER)
+# Choose palette
+palette = TolYlOrBr[9]
+# Tell Bokeh to use df4 as the source of the data
+source = ColumnDataSource(data=df4)
+# Define color mapper - which column will define the colour of the data points
+color_mapper = linear_cmap(field_name = 'NewDeaths', palette = palette, low = df4['NewDeaths'].min(), high = df4['NewDeaths'].max())
+# Set tooltips HoverTool
+tooltips = [("Country","@name"),("New Deaths","@NewDeaths"), ("Cumulative Deaths","@TotalDeaths")]
+p1 = figure(title = 'World Map',
+           x_axis_type="mercator", y_axis_type="mercator",
+           x_axis_label = 'Longitude', y_axis_label = 'Latitude', tooltips = tooltips)
+# Add map tile
+p1.add_tile(chosentile)
+# Add points using mercator coordinates
+p1.circle(x = 'mercator_x', y = 'mercator_y', color = color_mapper, source=source, size=15, fill_alpha = 0.7)
+#Defines color bar
+color_bar = ColorBar(color_mapper=color_mapper['transform'], 
+                     formatter = NumeralTickFormatter(format='0.0[0000]'), 
+                     label_standoff = 13, width=8, location=(0,0))
+# Set color_bar location
+p1.add_layout(color_bar, 'right')
+
+# FIGURE 2: Interactive cumulative death plot of top 5 countries
+p2 = figure(title='click on legend entries to hide country data', y_axis_label='Number of Deaths', height=400, x_axis_type='datetime')
+# add data to the figure
+country_names = df1["Country"].head(10)
+total_deaths1 = df1['Total Deaths'].head(10)
+total_deaths2 = df2['Total Deaths'].head(10)
+total_deaths3 = df3['Total Deaths'].head(10)
+df1['Date'] = pd.to_datetime(df1['Date'])
+df2['Date'] = pd.to_datetime(df2['Date'])
+df3['Date'] = pd.to_datetime(df3['Date'])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[0], total_deaths2[0],total_deaths3[0]],fill_color='red',size=8,legend_label=df1['Country'][0])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[1], total_deaths2[1],total_deaths3[1]],fill_color='orange',size=8,legend_label=df1['Country'][1])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[2], total_deaths2[2],total_deaths3[2]],fill_color='yellow',size=8,legend_label=df1['Country'][2])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[3], total_deaths2[3],total_deaths3[3]],fill_color='green',size=8,legend_label=df1['Country'][3])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[4], total_deaths2[4],total_deaths3[4]],fill_color='blue',size=8,legend_label=df1['Country'][4])
+
+p2.legend.location = 'top_left'
+p2.legend.click_policy='hide'
+# arrange these using the gridplot function
+layout = gridplot([
+    [p1,None],
+    [t1,p2]
+])
+
+show(layout)
