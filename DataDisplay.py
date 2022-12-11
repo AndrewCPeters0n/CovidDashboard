cfa176ef5def0e38fe6d229865d0cc1a8685c341
michael <97482177+notmichaellopez@users.noreply.github.com>
2022-12-11 13:33:02 -0700

    Add files via upload
    - Updated the legend of the last figure to indicate the correct country instead of the top countries listed in our dataframes.
    - Added a file location string that allows others to update one location and run on different computers.

--- DataDisplay.py
+++ DataDisplay.py
@@ -8,14 +8,17 @@ from bokeh.io import show
 from datetime import date, timedelta
 import pandas as pd
 import numpy as np
-# Run ScrapeWebsite.py in order to get new data
+# Run ScrapeWebsite.py before running this code to get the latest COVID data
 
 
-
+file_location = fr'C:path\to\JSON\file'
+string_1 = (fr"{file_location}\covid_data_{str(date.today() - timedelta(days = 1))}.json")
+string_2 = (fr"{file_location}\covid_data_{str(date.today() - timedelta(days = 2))}.json")
+string_3 = (fr"{file_location}\covid_data_{str(date.today() - timedelta(days = 3))}.json")
 # Load the last three json files of data using pandas
-df1 = pd.read_json(r"C:\Users\lopez\Desktop\CovidDashboard-main\covid_data_2022-12-02.json")
-df2 = pd.read_json(r"C:\Users\lopez\Desktop\CovidDashboard-main\covid_data_2022-12-01.json")
-df3 = pd.read_json(r"C:\Users\lopez\Desktop\CovidDashboard-main\covid_data_2022-12-10.json")
+df1 = pd.read_json(string_1)
+df2 = pd.read_json(string_2)
+df3 = pd.read_json(string_3)
 
 # TABLE 1: Table giving data from yesterday about all the columns collected
 # Tell Bokeh to use df1 as our source for the table                                                                                                  
@@ -32,7 +35,7 @@ t1 = DataTable(source=source,columns=columns)
 
 # FIGURE 1: World map with interactive new deaths by hovering 
 # Load the coordinate data from countries.csv
-df4 = pd.read_csv('updated_countries.csv',encoding='latin-1')
+df4 = pd.read_csv(r'updated_countries.csv')
 # Run the updated_countries script to get the coordinates of the top 100 covid countries
 
 
@@ -84,19 +87,29 @@ p1.add_layout(color_bar, 'right')
 
 # FIGURE 2: Interactive cumulative death plot of top 5 countries
 p2 = figure(title='click on legend entries to hide country data', y_axis_label='Number of Deaths', height=400, x_axis_type='datetime')
+# rearrange the data so new deaths is sorted from high to low
+df1 = df1.sort_values(by='New Deaths', ascending=False)
+df2 = df2.sort_values(by='New Deaths', ascending=False)
+df3 = df3.sort_values(by='New Deaths', ascending=False)
 # add data to the figure
-country_names = df1["Country"].head(10)
-total_deaths1 = df1['Total Deaths'].head(10)
-total_deaths2 = df2['Total Deaths'].head(10)
-total_deaths3 = df3['Total Deaths'].head(10)
+top_countries = df1['Country'].head(5).to_list()
+total_deaths1 = df1['New Deaths'].head(10).to_list()
+total_deaths2 = df2['New Deaths'].head(10).to_list()
+total_deaths3 = df3['New Deaths'].head(10).to_list()
 df1['Date'] = pd.to_datetime(df1['Date'])
 df2['Date'] = pd.to_datetime(df2['Date'])
 df3['Date'] = pd.to_datetime(df3['Date'])
-p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[0], total_deaths2[0],total_deaths3[0]],fill_color='red',size=8,legend_label=df1['Country'][0])
-p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[1], total_deaths2[1],total_deaths3[1]],fill_color='orange',size=8,legend_label=df1['Country'][1])
-p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[2], total_deaths2[2],total_deaths3[2]],fill_color='yellow',size=8,legend_label=df1['Country'][2])
-p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[3], total_deaths2[3],total_deaths3[3]],fill_color='green',size=8,legend_label=df1['Country'][3])
-p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[4], total_deaths2[4],total_deaths3[4]],fill_color='blue',size=8,legend_label=df1['Country'][4])
+
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[0], total_deaths2[0],total_deaths3[0]],
+          fill_color='red',size=8,legend_label=top_countries[0])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[1], total_deaths2[1],total_deaths3[1]],
+          fill_color='orange',size=8,legend_label=top_countries[1])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[2], total_deaths2[2],total_deaths3[2]],
+          fill_color='yellow',size=8,legend_label=top_countries[2])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[3], total_deaths2[3],total_deaths3[3]],
+          fill_color='green',size=8,legend_label=top_countries[3])
+p2.circle([df1['Date'][0],df2['Date'][0],df3['Date'][0]],[total_deaths1[4], total_deaths2[4],total_deaths3[4]],
+          fill_color='blue',size=8,legend_label=top_countries[4])
 
 p2.legend.location = 'top_left'
 p2.legend.click_policy='hide'
